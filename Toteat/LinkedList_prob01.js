dbc9a69dd7dcbb3556ed6af8046b104de25830a7
mraballard <mraballard@gmail.com>
2017-05-11 17:55:21 -0300

    Problem 1

---
+++ Toteat/LinkedList_prob01.js
@@ -0,0 +1,31 @@
+/*
+    Detect a cycle in a Linked List.
+    Note that the head may be 'null' if the list is empty.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function hasCycle(head) {
+    if (!head) {
+        return false;
+    };
+
+    let currentNode = head,
+        aggregateData = [];
+    while (currentNode.next) {
+        let loop = aggregateData.find(function(el) {
+            return el == currentNode.data;
+        });
+        if (loop) {return true;}
+        else {
+            aggregateData.push(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+}
